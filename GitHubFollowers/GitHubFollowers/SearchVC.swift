e4f26bdf9bfcefcb9c2aa3b5c128c76914f4443e
DerenB <dbozerprogramming@gmail.com>
2024-06-28 15:11:12 -0400

    Creating Search screen

--- GitHubFollowers/GitHubFollowers/SearchVC.swift
+++ GitHubFollowers/GitHubFollowers/SearchVC.swift
@@ -9,9 +9,79 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    /// Initialize UI Objects
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    
+    /// Gets called only on the first appearance
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemPink
+        /// Background Color
+        view.backgroundColor = .systemBackground
+        
+        /// Call Logo function
+        configureLogoImageView()
+        
+        /// Call the text field function
+        configureTextField()
+        
+        /// Call the Button function
+        configureCallToActionButton()
+    }
+    
+    
+    /// Gets called every time the View appears
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    
+    /// Function for adding the image
+    func configureLogoImageView() {
+        /// Equivalent to drag/drop UI item onto storyboard
+        view.addSubview(logoImageView)
+        
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        /// Loads image
+        logoImageView.image = UIImage(named: "gh-logo")
+        
+        /// Constraints
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    
+    /// Function for adding the text field
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    /// Function for adding the Button
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
