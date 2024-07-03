1688ea4a522ebee7759b632deab6996f5b6683d3
Deren B <47374311+DerenB@users.noreply.github.com>
2024-07-03 19:34:16 -0400

    view layout

--- GitHubFollowers/GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/GitHubFollowers/Screens/FollowerListVC.swift
@@ -12,19 +12,57 @@ class FollowerListVC: UIViewController {
     
     /// Username for passing the data
     var username: String!
+    
+    /// Collection View
+    var collectionView: UICollectionView!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+    /// View Controller settings
+    func configureViewController() {
         /// Background Color
         view.backgroundColor = .systemBackground
         
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+ 
+    /// Collection View
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    /// 3 Column layout settup
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
         
-        
-        /// Test network call
+        return UICollectionViewFlowLayout()
+    }
+    
+    
+    /// Testing the network call
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
             
             switch(result) {
@@ -36,12 +74,4 @@ class FollowerListVC: UIViewController {
             }
         }
     }
-
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
-    }
-    
-    
 }
