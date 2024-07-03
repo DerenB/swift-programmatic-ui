c31bdf03ea0055d2c76aa76342cc7da227cf5460
Deren B <47374311+DerenB@users.noreply.github.com>
2024-07-02 22:11:30 -0400

    Follow cell items

---
+++ GitHubFollowers/GitHubFollowers/CustomViews/Cells/FollowerCell.swift
@@ -0,0 +1,53 @@
+//
+//  FollowerCell.swift
+//  GitHubFollowers
+//
+//  Created by Deren Bozer on 7/2/24.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+    static let reuseID = "FollowerCell"
+    /// UI Elements
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    /// Sets the follower at index path
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
---
+++ GitHubFollowers/GitHubFollowers/CustomViews/GFAvatarImageView.swift
@@ -0,0 +1,40 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Deren Bozer on 7/2/24.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    /// Placeholder image
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        /// Corner Radius
+        /// Clips to bounds makes the image have a rounded corner
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        /// Placeholder Image
+        image = placeholderImage
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        
+    }
+}
